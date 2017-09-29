73fafe6ec4ea435111d01782e91feb8fc9ac6b9e
EArehon <earehon@gmail.com>
2017-09-29 15:00:20 +0300

    5!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let n = 5;
+  let result = 1;
+  var zeros = 0;				// количество нулей
+  let pos = 0;		  		// стартавая позиция для поиска нулей
+  
+  
+  
+  for(let i = 1; i <= n; i++){
+    result *= i;
+  }
+  
+  
+  
+  // считаем нули
+  while(true){ 
+    let foundPos = String(result).indexOf(0, pos);
+    if (foundPos == -1) break;
+    
+    zeros++;
+    pos = foundPos + 1;
+  }
+  
+  return zeros;
 }
